382a7855137754929210e11663f78414b212c577
Steve Dickinson <steve@Colossus.local>
2020-01-02 00:40:44 -0500

    Added files to new repo.

---
+++ v0.1/ChmodPro.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
@@ -0,0 +1,6 @@
+var localizedStrings = new Object;
+
+localizedStrings["Hello, World!"] = 'UNIX/Linux Permissions';
+localizedStrings["Done"] = "Done";
+localizedStrings[''] = '- UNIX/Linux Permissions Professor';
+localizedStrings['ChmodPro'] = 'ChmodPro';
